a8aa9b610e100654e3262ce14a4e62937d040d39
Maghfoor Ahmed <maghfoor.ahmed05@gmail.com>
2023-01-07 13:58:23 +0000

    initial commit

---
+++ .README.md
@@ -0,0 +1,20 @@
+# Express-TypeScript-RESTAPI-Template
+
+This template allows you to get started with building your REST API using Typescript and Express.
+
+## Getting started
+
+### Install all dependencies
+```
+yarn
+```
+
+### Run server
+```
+yarn start
+```
+
+Or, for dev mode (restarts server automatically when you save changes):
+```
+yarn start:dev
+```
---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import filePath from "./filePath";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+dotenv.config();
+
+const PORT_NUMBER = process.env.PORT ?? 4000;
+
+app.get("/", (req, res) => {
+    const pathToFile = filePath("../public/index.html");
+    res.sendFile(pathToFile)
+})
+
+app.listen(PORT_NUMBER, () => {
+    console.log(`Server is listneing on port ${PORT_NUMBER}!`);
+})
